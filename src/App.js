7dc86ed89688bb08e62523afe9d08cae4f4f7c69
Q42-KoraD <159777112+Q42-KoraD@users.noreply.github.com>
2024-05-08 14:15:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          OH CANARYYYYYY OH CANARYYYYYY :D
+          OH CANARYYYYYY :D
         </p>
         <a
           className="App-link"
