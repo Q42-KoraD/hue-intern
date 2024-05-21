c373de5316b7217292bed88544c0a084035965ce
Q42-KoraD <159777112+Q42-KoraD@users.noreply.github.com>
2024-05-21 13:39:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Fasegesprek 3! : DEMO NIEUWE DEPLOYMENT
+          Fasegesprek 3! : V2
         </p>
         <a
           className="App-link"
