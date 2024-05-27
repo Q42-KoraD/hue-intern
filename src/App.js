ea5791f5916ac7499347e757516a2bafbb99bf51
Q42-KoraD <159777112+Q42-KoraD@users.noreply.github.com>
2024-05-27 13:39:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Argo CD en Argo Rollouts demo : BLUESTEEL
+          Argo CD en Argo Rollouts demo : BLUEEEE
         </p>
         <a
           className="App-link"
